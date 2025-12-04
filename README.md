a7e6caa34ae63d0ddd7f63329da7ece54f07718d
369webforall <369webforall@gmail.com>
2025-12-04 12:35:20 +0545

    python setup and operator class

--- README.md
+++
@@ -1,3 +0,0 @@
-# python-class
-
-## In the folder you will find all the code related to python class.
---
+++ python_1/operator.py
@@ -0,0 +1,65 @@
+# string concatination
+
+print("hello" + " World")
+
+# Math operator
+# +, -, *, /, %, //, **
+
+print(5+5)
+
+result = (5+5) - 2 * 6 / 2
+
+print(result)
+
+print(7 % 2) # 1
+
+print(6 % 2) # 0
+
+print( 7 // 2) 
+
+print(2**3)
+
+# Assignment operator
+
+# = += -= *=
+
+x = 10
+
+y = 20
+
+x = x + 10
+
+x += 10
+
+print(x)
+
+dev = "Robert Welker"
+
+print(dev)
+
+# comperison operator
+# >, >=, <, <=, !=, == , return boolean, True or False
+
+print( 4 >= 5) # False
+print( 5 == 5) 
+print( 5 != 5)
+
+# Logical operator
+
+# and , or, not
+
+# and operator
+
+value = 5 > 4 and 6 > 4 # True
+
+# And must have both side true to get True
+
+# or as long one true your get ture
+
+print (True or False)
+
+print(True and False)
+
+print(not True)
+
+print (False or True)
